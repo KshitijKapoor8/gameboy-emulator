1d46d53d4e5cf52f98fc14c408c532125189ed26
KshitijKapoor8 <kshitijkapoor8@gmail.com>
2025-11-12 23:17:16 -0600

    feat(roms): added roms loading feat(cpu): added several more instructions including prefix instructions, but need to be verified

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,33 @@
 const std = @import("std");
-const expect = std.testing.expect;
 const windowing = @import("window.zig");
+const loader = @import("loader.zig");
+const expect = std.testing.expect;
 
 comptime {
     _ = @import("system_bus.zig");
 }
 
 pub fn main() !void {
-    try windowing.makeWindow(640, 380);
+    // try windowing.makeWindow(640, 380);
+
+    var bus = @import("system_bus.zig").g_test_system_bus;
+    try bus.init(bus.mappings);
+    const rom_buf = try loader.loadRomFromRomsDir();
+    for (rom_buf, 0..0x100) |b, i| {
+        try bus.write(@intCast(i), b);
+    }
+
+    std.debug.print("=== Game Boy Emulator - Boot ROM Verification ===\n\n", .{});
+
+    var cpu = @import("cpu.zig").CPU.init();
+
+    // comptime var cycles = 0;
+    for (0..100) |_| {
+        _ = try cpu.step(&bus);
+    }
+
+    std.debug.print("{}", .{cpu});
+
     return;
 }
 
