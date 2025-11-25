ddbce4be16f0007d17eea6f0748aa78d68369b33
karnikachoudhury <karnika.choudhury@gmail.com>
2025-11-24 19:52:36 -0600

    feat(cartridge): successfully loads the correct amount of cartridge bytes

--- src/loader.zig
+++ src/loader.zig
@@ -7,7 +7,7 @@ pub fn loadRomFromRomsDir() ![0x100]u8 {
     var stdin_buffer: [1024]u8 = undefined;
     var stdin = std.fs.File.stdin().reader(&stdin_buffer);
 
-    var dir = try cwd.openDir("roms", .{ .iterate = true });
+    var dir = try cwd.openDir("../roms", .{ .iterate = true });
     defer dir.close();
 
     const max_roms = 64;
@@ -50,7 +50,7 @@ pub fn loadRomFromRomsDir() ![0x100]u8 {
 
     const rom_name = rom_names[selection];
 
-    const full_path = try std.fs.path.join(allocator, &.{ "roms", rom_name });
+    const full_path = try std.fs.path.join(allocator, &.{ "../roms", rom_name });
     var file = try cwd.openFile(full_path, .{ .mode = .read_only });
     defer file.close();
 
@@ -76,6 +76,70 @@ pub fn loadFixed256Rom(file_path: []const u8) ![0x100]u8 {
     const n = try file.readAll(&buf);
     if (n != buf.len)
         return error.FileTooSmall;
+
+    return buf;
+}
+
+// loading cartridge from allocator because we need to chekc file size and allocate buffer
+pub fn loadCartRomAlloc(allocator: std.mem.Allocator) ![]u8 {
+    var cwd = std.fs.cwd();
+
+    var stdin_buffer: [1024]u8 = undefined;
+    var stdin = std.fs.File.stdin().reader(&stdin_buffer);
+
+    var dir = try cwd.openDir("../roms", .{ .iterate = true });
+    defer dir.close();
+
+    const max_roms = 64;
+    var rom_names: [max_roms][]const u8 = undefined;
+    var rom_count: usize = 0;
+
+    var names_buf: [4096]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&names_buf);
+    const allocator_names = fba.allocator();
+
+    var it = dir.iterate();
+    while (try it.next()) |entry| {
+        if (entry.kind != .file)
+            continue;
+        if (rom_count >= max_roms)
+            return error.TooManyRoms;
+
+        const name_copy = try allocator_names.dupe(u8, entry.name);
+        rom_names[rom_count] = name_copy;
+        rom_count += 1;
+    }
+
+    std.debug.print("Available ROMs in ./roms for Cartridge:\n", .{});
+    var i: usize = 0;
+    while (i < rom_count) : (i += 1) {
+        std.debug.print("  {d}) {s}\n", .{ i, rom_names[i] });
+    }
+
+    std.debug.print("Select a ROM number: ", .{});
+
+    var input_buf: [1]u8 = undefined;
+    _ = try stdin.interface.readSliceAll(&input_buf);
+
+    const selection = try std.fmt.parseInt(usize, &input_buf, 10);
+    if (selection >= rom_count)
+        return error.InvalidSelection;
+
+    const rom_name = rom_names[selection];
+
+    const full_path = try std.fs.path.join(allocator_names, &.{ "../roms", rom_name });
+    var file = try cwd.openFile(full_path, .{ .mode = .read_only });
+    defer file.close();
+
+    // Determine file size, allocate exactly that much
+    const stat = try file.stat();
+    const rom_size = @as(usize, @intCast(stat.size));
+
+    const buf = try allocator.alloc(u8, rom_size);
+    const n = try file.readAll(buf);
+    if (n != rom_size) return error.ShortRead;
+
+    std.debug.print("Read {d} bytes from {s}\n", .{ n, rom_name });
 
     return buf;
 }
--- src/main.zig
+++ src/main.zig
@@ -6,48 +6,37 @@ const BootRom = @import("devices/bootrom.zig").BootRom;
 const Cartridge = @import("devices/cartridge.zig").Cartridge;
 const cart_mmio = @import("devices/cart_boot_mmio.zig");
 
-
 pub fn main() !void {
-   // try windowing.makeWindow(640, 380);
+    // try windowing.makeWindow(640, 380);
 
+    var bus = @import("system_bus.zig").g_test_system_bus;
+    try bus.init(bus.mappings);
+    const rom_buf = try loader.loadRomFromRomsDir();
+    cart_mmio.setBus(&bus.bus.mem);
+    var boot = BootRom.init(rom_buf[0..]);
+    for (rom_buf, 0..0x100) |b, i| {
+        try bus.write(@intCast(i), b);
+    }
+    const gpa = std.heap.page_allocator;
+    const cart_bytes = try loader.loadCartRomAlloc(gpa);
+    var cart = Cartridge.init(cart_bytes);
 
-   var bus = @import("system_bus.zig").g_test_system_bus;
-   try bus.init(bus.mappings);
-   const rom_buf = try loader.loadRomFromRomsDir();
-   var boot = BootRom.init(rom_buf[0..]);
-   for (rom_buf, 0..0x100) |b, i| {
-       try bus.write(@intCast(i), b);
-   }
-   const gpa = std.heap.page_allocator;
-   const cart_bytes = try loader.loadCartRomAlloc(gpa);
-   var cart = Cartridge.init(cart_bytes);
+    cart_mmio.setCartridgeBoot(&cart, &boot);
 
+    std.debug.print("=== Game Boy Emulator - Boot ROM Verification ===\n\n", .{});
 
-   cart_mmio.setCartridgeBoot(&cart, &boot);
+    var cpu = @import("cpu.zig").CPU.init();
 
+    // comptime var cycles = 0;
+    for (0..0x100) |_| {
+        _ = try cpu.step(&bus);
+    }
 
-   std.debug.print("=== Game Boy Emulator - Boot ROM Verification ===\n\n", .{});
+    std.debug.print("{}", .{cpu});
 
-
-   var cpu = @import("cpu.zig").CPU.init();
-
-
-   // comptime var cycles = 0;
-   for (0..0x100) |_| {
-       _ = try cpu.step(&bus);
-   }
-
-
-   std.debug.print("{}", .{cpu});
-
-
-   return;
+    return;
 }
-
 
 test "Basic test" {
-   try expect(1 == 1);
+    try expect(1 == 1);
 }
-
-
-
